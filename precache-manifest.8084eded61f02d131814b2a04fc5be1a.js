88eaa1b6469811a4ddcd1e81342c93a4effb774d
tan <nguyentan15102000@gmail.com>
2020-06-24 22:18:38 +0700

    Updates

---
+++ precache-manifest.8084eded61f02d131814b2a04fc5be1a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9607290dead25ea29a0f8eb50bfb0eae",
+    "url": "/myprofile/index.html"
+  },
+  {
+    "revision": "77677fc7cfcc0249d106",
+    "url": "/myprofile/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "afa49dcc981b02cde325",
+    "url": "/myprofile/static/js/2.e4121552.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/myprofile/static/js/2.e4121552.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "77677fc7cfcc0249d106",
+    "url": "/myprofile/static/js/main.f5b30b96.chunk.js"
+  },
+  {
+    "revision": "bfafe7cbdcc7babe7919",
+    "url": "/myprofile/static/js/runtime-main.7af89b8f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/myprofile/static/media/logo.5d5d9eef.svg"
+  }
+]);
