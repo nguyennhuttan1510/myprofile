077848af1153ce4309d3c72a8c8c1bb47d1dcc95
tan <nguyentan15102000@gmail.com>
2020-07-03 12:54:46 +0700

    Updates

---
+++ precache-manifest.007187d07c6dcb6fae182cb9adc4b2bb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e972a9f9f95ce24fb321f352c668df4",
+    "url": "/myprofile/index.html"
+  },
+  {
+    "revision": "e8c9ff79a4c2729c27a4",
+    "url": "/myprofile/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "d93caf0be78b473f77e6",
+    "url": "/myprofile/static/js/2.f7285924.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/myprofile/static/js/2.f7285924.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e8c9ff79a4c2729c27a4",
+    "url": "/myprofile/static/js/main.1a594976.chunk.js"
+  },
+  {
+    "revision": "bfafe7cbdcc7babe7919",
+    "url": "/myprofile/static/js/runtime-main.7af89b8f.js"
+  },
+  {
+    "revision": "379d4f36305a2817c0e510d44e6c678f",
+    "url": "/myprofile/static/media/3.379d4f36.jpg"
+  }
+]);
